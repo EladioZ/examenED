f81beb8a0b7af823e5d03abdc0d4a1ccd189d677
EladioZ <34168896+EladioZ@users.noreply.github.com>
2017-12-15 16:55:28 +0100

    issue1

---
+++ src/ELADIO_ZAMORA/ExamenEDRefactorizado.java
@@ -0,0 +1,52 @@
+package ELADIO_ZAMORA;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ExamenEDRefactorizado {
+
+    static int w = 30;
+
+    public static void main(String args[]){
+
+        System.out.println("INTRODUCE UN NUMERO: ");
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        Random random = new Random();
+        int []a = new int[m];
+        for(int in=0; in<m; in++)
+        {
+            a[in] = random.nextInt(ExamenEDRefactorizado.w);
+        }
+
+        ExamenEDRefactorizado.ejecutarAlgoritmo(a);
+        input.close();
+    }
+
+
+    public static void ejecutarAlgoritmo(int[] a){
+
+        int x = 0;
+        int y = ExamenEDRefactorizado.w;
+
+        for(int in=0; in<a.length; in++)
+        {
+            System.out.println(a[in]);
+            if(a[in] > x ){
+                x = a[in];
+            }
+
+            if(a[in] < y){
+                y = a[in];
+            }
+        }
+
+        int z = y *x;
+        if (z == 0 || (z % 2 != 0)  ){
+            System.out.println("NO ES");
+        }else{
+            System.out.println("ES");
+        }
+
+    }
+}
